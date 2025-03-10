3a8af21a9103809f1186a9a4327ba58ddbd88c78
Rudolfs Pukitis <pukitis.rudolfs@gmail.com>
2025-03-10 10:25:41 +0200

    updates imports

--- apps/address-book-minimal/src/main.tsx
+++ apps/address-book-minimal/src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import './index.css'
-import Home from './Home.tsx'
+import '@/index.css'
+import Home from '@/Home.tsx'
 
 const queryClient = new QueryClient()
 
